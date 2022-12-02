546d86f7ecf857a0ad29e337dbd364f9f923d1ec
WilliamStar <anda.liang@vanderbilt.edu>
2022-12-02 00:08:33 -0600

    Update file.js

--- static/js/file.js
+++ static/js/file.js
@@ -1,7 +1,7 @@
 // adapted from https://dmitripavlutin.com/javascript-fetch-async-await/
 
 async function loadFile(filename) {
-    return await fetch(`../static/data/${user}.csv`).then(function (response) {
+    return await fetch(`../static/data/${filename}.csv`).then(function (response) {
         if (response.status !== 200) {
             throw response.status;
         }
