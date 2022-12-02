8686efa66358818ec92f8ab87f83f345300e905d
WilliamStar <anda.liang@vanderbilt.edu>
2022-12-01 19:35:43 -0600

    updates

---
+++ static/js/query.js
@@ -0,0 +1,20 @@
+// function adapted from https://www.sitepoint.com/get-url-parameters-with-javascript/
+
+function query() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    return urlParams.get('surveyID');
+}
+
+function redirect() {
+    const surveyID = query();
+    const root = "https://williamstar007.github.io/Information-Awareness-Survey";
+
+    if (document.getElementById("consent").checked)
+        window.location.href = `${root}/templates/surveys/basic.html/?surveyID=${surveyID}`;
+    else
+        window.location.href = `${root}/templates/end.html`;
+
+    return false;
+}
